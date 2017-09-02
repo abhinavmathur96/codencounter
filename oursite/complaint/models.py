ec856a8c9ee0f7a987928ef2ee25b68a189afaac
Devansh Bhatia <dbhrockzz1@gmail.com>
2017-09-02 17:21:40 +0530

    Models Changed

--- oursite/complaint/models.py
+++ oursite/complaint/models.py
@@ -2,12 +2,19 @@
 from __future__ import unicode_literals
 from django.db import models
 
+class resources(models.Model):
+    name = models.CharField(max_length=40)
+    department = models.CharField(max_length=20)
+    working = models.BooleanField()
+
+
 class complaint(models.Model):
     loc_choices = (
         ('23','SECTOR 23'),
         ('22','SECTOR 22'),
         ('14','SECTOR 14'),
     )
+    title = models.CharField(max_length = 140,default="")
     departments = models.CharField(max_length = 20)
     location = models.CharField(max_length=5, choices = loc_choices)
     severity = models.IntegerField()
@@ -15,6 +22,7 @@ class complaint(models.Model):
     image = models.ImageField()
     solution = models.TextField()
     created = models.DateTimeField(auto_now_add = True)
+    assign = models.ForeignKey(resources, related_name = 'using', default = None)
 
 class progress(models.Model):
     id = models.OneToOneField(complaint,related_name="progress",primary_key = True)
@@ -22,8 +30,4 @@ class progress(models.Model):
     completed = models.BooleanField(default = False)
     updated = models.DateTimeField(auto_now = True)
 
-class resources(models.Model):
-    name = models.CharField(max_length=40)
-    department = models.CharField(max_length=20)
-    working = models.BooleanField()
     
--- oursite/complaint/views.py
+++ oursite/complaint/views.py
@@ -2,7 +2,11 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.auth.models import *
+from .models import *
 
 def index(request):
-    return render(request, 'index.html', {'title': 'Home'})
+    in_progress = progress.objects.filter(completed=False).order_by('-update')
+    in_progress = in_progress[:min(5,len(in_progress))]
+    for i in range(len(in_progress)):
+        in_progress[i] = complaint.objects.get(id=in_progress[i].id).title
+    future = complaint.objects.filter(assign=null)
