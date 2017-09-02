0cf5b8073e60a72d25bbcad9e4ec12b8fc67d097
Devansh Bhatia <dbhrockzz1@gmail.com>
2017-09-02 17:38:05 +0530

    new New

--- oursite/complaint/views.py
+++ oursite/complaint/views.py
@@ -11,5 +11,7 @@ def index(request):
         in_progress[i] = complaint.objects.get(id=in_progress[i].id)
     future = complaint.objects.filter(assign=None).order_by('created')
     future = future[:min(5,len(future))]
-    recent = complaint.objects.filter().order_y
+    recent = complaint.objects.filter(completed=False).order_by('-created')
+    recent = recent[:min(5,len(recent))]
+    return render(request,'index.html',{'progress':in_progress,'future':future,'recent':recent})
 
