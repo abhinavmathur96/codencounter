d494babf59d583c755ef728f695998e80344c591
Abhinav Mathur <abhinavmathur96@gmail.com>
2017-09-03 05:26:20 +0530

    department

--- oursite/complaint/urls.py
+++ oursite/complaint/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
 	url(r'^details/(?P<id>\d+)', views.details, name='details'),
 	url(r'^department/login/$', views.dept_login, name="dept_login"),
 	url(r'^department/(?P<id>\d+)', views.department_id, name="department"),
+	url(r'^department/detail/(?P<id>\d+)', views.department_detail, name="department_detail"),
 	url(r'^new/', views.new_compl, name="new_compl"),
 ]
