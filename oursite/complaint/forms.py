e8bb08853f31047cf934f2f619ac584617292fd7
Devansh Bhatia <dbhrockzz1@gmail.com>
2017-09-02 19:15:50 +0530

    Auto stash before merge of "master" and "origin/master"
    Latest

---
+++ oursite/complaint/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+
+class ComplaintForm(forms.Form):
+    title = forms.CharField(label="Complaint Title",max_length=140)
+    DEPT_CHOICES = (
+        ('1','ELECTRICAL'),
+        ('2', 'WATER'),
+        ('3', 'WASTE'),
+    )
+    LOC_CHOICES = (
+        ('23','SECTOR 23'),
+        ('22','SECTOR 22'),
+        ('14','SECTOR 14'),
+    )
+    dept = forms.MultipleChoiceField(label="Choose Department(s)",required=True,widget=forms.CheckboxSelectMultiple,choices = DEPT_CHOICES)
+    location = forms.CharField(label="Choose Location",widget=forms.Select(choices=LOC_CHOICES))
+    severity = forms.IntegerField()
+    description = forms.TextField()
+    image = forms.ImageField()
+    solution = forms.TextField()
--- oursite/complaint/urls.py
+++ oursite/complaint/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name="index")
+	url(r'^details/(?P<id>\d+)', views.details, name='details')
+	url(r'^department/(?P<id>\d+)', views.department, name="department")
 ]
