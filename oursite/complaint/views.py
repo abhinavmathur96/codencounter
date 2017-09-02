4a519fad004cbf838debd013f113c82e896e915e
Devansh Bhatia <dbhrockzz1@gmail.com>
2017-09-02 17:28:49 +0530

    index

--- oursite/complaint/views.py
+++ oursite/complaint/views.py
@@ -5,8 +5,11 @@ from django.shortcuts import render
 from .models import *
 
 def index(request):
-    in_progress = progress.objects.filter(completed=False).order_by('-update')
+    in_progress = progress.objects.filter(completed=False).order_by('-updated')
     in_progress = in_progress[:min(5,len(in_progress))]
     for i in range(len(in_progress)):
-        in_progress[i] = complaint.objects.get(id=in_progress[i].id).title
-    future = complaint.objects.filter(assign=null)
+        in_progress[i] = complaint.objects.get(id=in_progress[i].id)
+    future = complaint.objects.filter(assign=None).order_by('created')
+    future = future[:min(5,len(future))]
+    recent = complaint.objects.filter().order_y
+
