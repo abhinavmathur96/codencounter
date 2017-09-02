f186f469d8161b65ed43e4330f2ddb87b758e023
Abhinav Mathur <abhinavmathur96@gmail.com>
2017-09-02 22:41:57 +0530

    remove flow

--- oursite/complaint/urls.py
+++ oursite/complaint/urls.py
@@ -1,9 +1,9 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
 from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name="index"),
 	url(r'^details/(?P<id>\d+)', views.details, name='details'),
 	url(r'^department/(?P<id>\d+)', views.department, name="department"),
-	url(r'^new/', views.new_compl, name="new_compl")
+	url(r'^new/', views.new_compl, name="new_compl"),
 ]
